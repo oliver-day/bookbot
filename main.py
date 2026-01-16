715fdc2d12c1a0a0863efc66633ac9030ffed8a2
oliver-day <oliver.day.swe@gmail.com>
2026-01-16 05:53:34 -0800

    add stats module

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from stats import get_num_words
+
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
 
     print(f"--- Begin report of {book_path} ---")
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
     print()
 
     character_count_map = get_character_count_from_text(text)
@@ -19,11 +22,6 @@ def main():
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
 
 
 def get_character_count_from_text(text):
